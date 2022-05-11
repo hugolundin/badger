9612b2de6770dadb8388c88f016e4b11c8164f86
Hugo Lundin <hugo@lundin.dev>
2022-05-11 15:51:27 +0200

    Add initial files

---
+++ badger/badger.py
@@ -0,0 +1,14 @@
+import logging
+log = logging.getLogger(__name__)
+
+import asyncio
+from zeroconf import IPVersion, ServiceInfo, Zeroconf
+
+from proxy import Proxy
+
+class Badger:
+    def __init__(self):
+        self.zeroconf = Zeroconf(ip_version=IPVersion.All)
+
+    async def run(self):
+        await asyncio.Event().wait()
---
+++ badger/cli.py
@@ -0,0 +1,40 @@
+import logging, coloredlogs
+log = logging.getLogger(__name__)
+
+import sys
+import click
+import asyncio
+from signal import SIGINT, SIGTERM
+from setproctitle import setproctitle, setthreadtitle
+
+from badger import Badger
+
+def cancel():
+    for t in asyncio.all_tasks():
+        t.cancel()
+
+@click.command()
+@click.option('--level', type=click.Choice(coloredlogs.find_defined_levels().keys()), default='WARNING')
+@click.pass_context
+def main(ctx, level):
+    coloredlogs.install(
+        stream=sys.stdout,
+        fmt='%(asctime)s %(levelname)s %(message)s',
+        level=coloredlogs.find_defined_levels()[level])
+
+    setproctitle(ctx.command_path)
+
+    badger = Badger()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    
+    for s in [SIGINT, SIGTERM]:
+        loop.add_signal_handler(s, cancel)
+
+    try:
+       loop.run_until_complete(badger.run())
+    except asyncio.exceptions.CancelledError:
+        pass
+    finally:
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
