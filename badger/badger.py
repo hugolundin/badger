39c2d6b13ab9a2775af9909ae5cc9e3d6457a9cf
Hugo Lundin <hugo@lundin.dev>
2022-05-12 07:31:38 +0200

    Work on proxy implementation

--- badger/badger.py
+++ badger/badger.py
@@ -1,15 +1,40 @@
 import logging
 log = logging.getLogger(__name__)
 
-import asyncio
+import asyncio, socket
+from result import Ok, Err
 from zeroconf import IPVersion, ServiceInfo, Zeroconf
 
 from .proxy import Proxy
 
 class Badger:
     def __init__(self):
+        self.mappings = {'test': ('127.0.0.1', 8000)}
         self.proxy = Proxy()
         self.zeroconf = Zeroconf(ip_version=IPVersion.All)
 
+    def __del__(self):
+        self.zeroconf.unregister_all_services()
+        self.zeroconf.close()
+
     async def run(self):
-        await asyncio.Event().wait()
+        await self.register_mappings()
+
+        match await self.proxy.run(self.mappings):
+            case Ok(_):
+                pass
+            case Err(ret):
+                log.error(f'Proxy returned {ret}')
+
+    async def register_mappings(self):
+        for name, _ in self.mappings.items():
+            service = ServiceInfo(
+                '_http._tcp.local.',
+                f'{name}._http._tcp.local.',
+                addresses=[socket.inet_aton('10.0.0.4')],
+                port=80,
+                properties={},
+                server=f'{name}.local',
+            )
+
+            self.zeroconf.register_service(service)
--- badger/proxy/proxy.py
+++ badger/proxy/proxy.py
@@ -1,3 +1,32 @@
+import logging
+log = logging.getLogger(__name__)
+
+import json
+import asyncio
+from result import Result, Ok, Err
 
 class Proxy:
-    pass
+    def __init__(self):
+        pass
+
+    async def run(self, mappings) -> Result[None, None]:
+        serialized = json.dumps([
+            {'name': name, 'host': host, 'port': port} for name, (host, port) in mappings.items()
+        ])
+
+        process = await asyncio.create_subprocess_shell(
+            f'mitmdump -p 80 -s handler.py --quiet --set mappings={serialized}',
+            shell=True
+        )
+
+        log.debug(f'Started {process.pid}')
+
+        stdout, stderr = await process.communicate()
+
+        log.debug(f'{stdout} {stderr}')
+
+        match process.returncode:
+            case 0:
+                return Ok()
+            case code:
+                return Err(code)
